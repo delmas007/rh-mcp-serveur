04de876184b1cf6b68fe596f0c4351746a5a928c
delmas007 <azertyazerty1ze@gmail.com>
2025-06-18 08:22:39 +0000

    init

---
+++ src/main/java/com/example/rhmcpserveur/RhMcpServeurApplication.java
@@ -0,0 +1,13 @@
+package com.example.rhmcpserveur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhMcpServeurApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RhMcpServeurApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rhmcpserveur/RhMcpServeurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rhmcpserveur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhMcpServeurApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
